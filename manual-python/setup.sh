d6291b2e6264a6fde6e7994367594ba56af812cb
Josephine Stark <j.stark.grad@gmail.com>
2026-08-02 00:37:33 -0400

    update sklearn -> scikit-learn in environment.yml

--- manual-python/setup.sh
+++ manual-python/setup.sh
@@ -5,10 +5,10 @@ module load miniconda3/24.1.2-py310
 conda env create -f environment.yml
 
 #activate environment
-conda activate 7030_class_4
+conda activate 7030_class_5
 
 #register python kernel in jupyter
-python -m ipykernel install --user --name 7030_class_4 --display-name "Python (7030_class_4)"
+python -m ipykernel install --user --name 7030_class_5 --display-name "Python (7030_class_4)"
 
 #Starts jupyterlab (can not run from inside jupyter lab)
 jupyter lab --no-browser --port=2000
