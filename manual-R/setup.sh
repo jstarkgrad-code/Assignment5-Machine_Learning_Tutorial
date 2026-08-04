048808b72824462ab65fae198d96a37c822ae52e
Josephine Stark <j.stark.grad@gmail.com>
2026-08-03 22:07:19 -0400

    add begining of R tutorial - environment and notebook.

---
+++ manual-R/setup.sh
@@ -0,0 +1,14 @@
+#load miniconda
+module load miniconda3/24.1.2-py310
+
+#create environment from .yml
+conda env create -f environment.yml
+
+#activate environment
+conda activate 7030_class_5
+
+#register python kernel in jupyter
+python -m ipykernel install --user --name 7030_class_5 --display-name "Python (7030_class_4)"
+
+#Starts jupyterlab (can not run from inside jupyter lab)
+jupyter lab --no-browser --port=2000
